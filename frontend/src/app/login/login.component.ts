22861246d8da0582b05e5457503b1bace59146c9
Joaquin Cespedes <joaquin@jcespedes.dev>
2020-09-14 21:08:31 -0300

    Update login component

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -8,6 +8,9 @@ import {AuthenticationService} from '../../services/authentication/authenticatio
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
+  success = false;
+  loading = false;
+  error = null;
   loginForm: FormGroup = new FormGroup({
     email: new FormControl(),
     password: new FormControl(),
@@ -18,13 +21,19 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
   submit(): void {
+
+    this.loading = true;
+    this.error = null;
     const email = this.loginForm.get('email').value;
     const password = this.loginForm.get('password').value;
     this.authenticationService.login(email, password).subscribe(
       user => {
+        this.success = true;
         console.log(user);
       }, e => {
-        console.log(e);
+        this.loading = false;
+        this.error = e;
+        console.log('err', e);
       }
     );
   }
