64b5ce59fb64d64d14b5951b507f5039fa7a4881
Fabián <Mariqueo>
2020-09-17 00:16:00 -0300

    Coneccion con el backend

--- frontend/src/services/authentication/authentication.service.ts
+++ frontend/src/services/authentication/authentication.service.ts
@@ -1,42 +1,58 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {User} from '../../models/user';
-import {UserMocks} from '../../mocks/user-mocks';
-import {LocalStorageService} from 'ngx-webstorage';
-import {Observable, of, throwError} from 'rxjs';
-import {delay} from 'rxjs/operators';
+import { LocalStorageService } from 'ngx-webstorage';
+import { Observable, of, throwError } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { UserMocks } from 'src/mocks/user-mocks';
+import { User } from '../../models/user';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthenticationService {
-  authenticatedUser: User;
-  constructor(private localStorage: LocalStorageService) {}
-  login(email: string, password: string): Observable<User> {
-    const user = UserMocks.find(u => u.email === email && u.password === password);
-    if (user != null){
-      this.authenticatedUser = user;
-      this.localStorage.store('authenticatedUser', user);
-      return of(user).pipe(delay(3000));
-    } else {
-      return throwError('Usuario o contraseña inválidos');
+    authenticatedUser: User;
+    constructor(private localStorage: LocalStorageService, private http: HttpClient) { }
+
+    login(email: string, password: string): Observable<User> {
+        const user = UserMocks.find(u => u.email === email && u.password === password);
+        if (user != null) {
+            this.authenticatedUser = user;
+            this.localStorage.store('authenticatedUser', user);
+            return of(user).pipe(delay(3000));
+        } else {
+            return throwError('Usuario o contraseña inválidos');
+        }
+
+        // Formulario con datos de usuario        
     }
-  }
-  isLoggedIn(): boolean {
-    this.getUserInstance();
-    return this.authenticatedUser != null;
-  }
-  getLoggedUser(): User {
-    if (this.isLoggedIn()) {
-      return this.authenticatedUser;
-    }else{
-      return null;
+
+    loginOauth(email: string, password: string): Observable<User> {
+        const form: FormData = new FormData();
+        form.set('grant_type', 'password');
+        form.set('username', email);
+        form.set('password', password);
+
+        // Enviar petición
+        return this.http.post<User>(environment.api + 'oauth/token', form);
     }
-  }
-  logout(): void {
-    this.authenticatedUser = null;
-    this.localStorage.clear('authenticatedUser');
-  }
-  getUserInstance(): void{
-    this.authenticatedUser = this.localStorage.retrieve('authenticatedUser');
-  }
+
+    isLoggedIn(): boolean {
+        this.getUserInstance();
+        return this.authenticatedUser != null;
+    }
+    getLoggedUser(): User {
+        if (this.isLoggedIn()) {
+            return this.authenticatedUser;
+        } else {
+            return null;
+        }
+    }
+    logout(): void {
+        this.authenticatedUser = null;
+        this.localStorage.clear('authenticatedUser');
+    }
+    getUserInstance(): void {
+        this.authenticatedUser = this.localStorage.retrieve('authenticatedUser');
+    }
 }
