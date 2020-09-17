742f9f243c8e06e6671db30055c812aaaf8b6d43
Matías Hermosilla <matiasahermosilla@gmail.com>
2020-09-17 09:03:03 -0300

    Update AuditingConfig.java

--- Backend/src/main/java/cl/ufro/auth/config/AuditingConfig.java
+++ Backend/src/main/java/cl/ufro/auth/config/AuditingConfig.java
@@ -1,8 +1,12 @@
 package cl.ufro.auth.config;
 
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.domain.AuditorAware;
 import cl.ufro.auth.model.Usuario;
 import cl.ufro.auth.service.AuditorAwareImpl;
@@ -14,6 +18,11 @@ public class AuditingConfig {
     @Bean
     public AuditorAware<Usuario> auditorAware() {
         return new AuditorAwareImpl();
+    }
+
+    @Bean
+    public DateTimeProvider dateTimeProvider() {
+        return () -> Optional.of(ZonedDateTime.now());
     }
 
 }
