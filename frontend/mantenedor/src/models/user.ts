5d580d6e109705abdc297f4b8ff12661099807f6
Fabián <Mariqueo>
2020-09-30 23:13:42 -0300

    correccion modelo frontend

--- frontend/mantenedor/src/models/user.ts
+++ frontend/mantenedor/src/models/user.ts
@@ -1,11 +1,20 @@
 export class User {
-  id: string;
-  username: string;
-  enabled: boolean;
-  createdAt: string;
-  authorities: string[];
-  password: string;
-  // name: string;
-  // email: string;
-  // type: number;
+    id: string;
+    address: string;
+    nickname: string;
+    username: string;
+    givenName: string;
+    locale: string = "es";
+    familyName: string;
+    middleName: string;
+    phone: string;
+    pictrure: string;
+    website: string;
+    enabled: boolean;
+    createdAt: string;
+    authorities: string[];
+    password: string;
+    // name: string;
+    // email: string;
+    // type: number;
 }
